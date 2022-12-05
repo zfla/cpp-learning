f0f2bd73ed1d26b50e34cc7266a522dac2edc481
Zain Fox-Latif <98270296+zfla@users.noreply.github.com>
2022-12-05 20:30:39 +0000

    formatting

--- program1/program1.cpp
+++ program1/program1.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-    int x;
+    int x{};
     std::cout << "Enter an integer: ";
     std::cin >> x;
-    int result = x * 2;
+
+    int result{ x * 2};
     std::cout << "Double that number is: " << result << std::endl;
+
     return 0;
 }
