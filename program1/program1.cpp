cddab53820bba58cf279c95521953ebc41e7fe24
Zain Fox-Latif <98270296+zfla@users.noreply.github.com>
2022-12-05 20:28:34 +0000

    finishing first program

---
+++ program1/program1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+    int result = x * 2;
+    std::cout << "Double that number is: " << result << std::endl;
+    return 0;
+}
